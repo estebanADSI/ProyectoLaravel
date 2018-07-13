4330e4bb6124f8a86e1031657aaf09e2a70c7846
Your Name <you@example.com>
2018-07-13 11:50:52 -0500

    se creo una nueva ruta llamada ruta2

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,15 @@ Route::get('/mi_ruta', function($name){
 });
 
 Route::get('/name/{name}', function($name){
-    return 'Hola soy '.$name;
+    return 'Hola  '.$name;
+});
+
+Route::get('/names/{name}', function($name){
+    return 'Hola  '.$name;
+});
+
+//--------------------------------------------------------
+
+Route::get('Ruta2', function(){
+    return 'Esta es la ruta dos';
 });
