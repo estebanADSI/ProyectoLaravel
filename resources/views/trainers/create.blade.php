241ad25217eba105995935c10029473362a2b68a
Your Name <you@example.com>
2018-07-13 16:36:31 -0500

    se creo la primer vista con un formulario

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi pagina</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
+</head>
+<body>
+    <center> 
+    
+    <div class="container">
+    <h1>Hola Soy la vista</h1>
+    <div class="form-group">
+        <label for="">Nombre</label>
+        <input type="text" class="form-control">
+    </div>
+    <button class="btn btn-danger" type="submit">Guardar</button>
+    </div>
+    </center>
+</body>
+</html>
