9a0a574914235ca9db4733b3e786855fe9671467
Your Name <you@example.com>
2018-07-17 09:50:33 -0500

    se creo un bootn el vista show para edditar al trainer

--- resources/views/trainers/show.blade.php
+++ resources/views/trainers/show.blade.php
@@ -9,6 +9,8 @@
     <img style="heigth:200px; width:300px; background-color:#EFEFEF; margin:25px;"  class="card-img-top rounded-circle mx-auto d-block" src="/images/{{$trainer->avatar}}" alt="Card image cap">
              <h5 class="card-title">{{$trainer->name}}</h5>
                          <p class="card-text">{{$trainer->description}}</p>
+                         
+                         <a href="/trainers/{{$trainer->slug}}/edit/" class="btn btn-outline-success">Editar</a>
      </div>
     
 @endsection
